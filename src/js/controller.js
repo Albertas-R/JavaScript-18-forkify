804f649b13822a7c5c01e748abbfc820eda20620
Albertas <albertas.rinkevicius@gmail.com>
2022-02-23 15:08:57 +0200

    287 end

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,43 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// 287 Loading a Recipe from API
+console.log(`---------- 287 Loading a Recipe from API ----------`);
+
+// 1. npm init (creates package.json file)
+// 2. npm i parcel@2 -D (install parcel) (install latest version npm i parcel -D)
+// 3. npm run start (start parcel) (arba be run -> npm start)
+// 4. npm install (jei kazko truksta arba klaidos)
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+    );
+    const data = await response.json();
+
+    if (!response.ok)
+      throw new Error(`🔔 ${data.message} (${response.status})`);
+
+    console.log(response);
+    console.log(data);
+
+    let { recipe } = data.data; // same as let recipe = data.data.recipe;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+showRecipe();
