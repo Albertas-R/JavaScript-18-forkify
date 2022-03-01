1a917a8909166fcdfedbea63c0d39417877a99fc
Albertas <albertas.rinkevicius@gmail.com>
2022-03-01 12:31:26 +0200

    296 end

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable'; // polyfiling everything else
 import 'regenerator-runtime/runtime'; // polifiling async await
 import { async } from 'regenerator-runtime';
+
+// this is not real JavaScript (module.hot) but for parcel
+if (module.hot) {
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -36,6 +42,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -44,9 +52,10 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    // console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
-    console.log(err);
+    resultsView.renderError();
   }
 };
 
