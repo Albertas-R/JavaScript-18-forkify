693fddc328b6f175cee373c15eac2a1515412c2e
Albertas <albertas.rinkevicius@gmail.com>
2022-03-04 21:09:57 +0200

    308 end

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-// export const KEY = 'bac04dde-0a79-4218-a640-e0475fc62edc';
-export const KEY = 'e9f452f4-1de1-442e-970b-cbc3aab4c173';
+// export const KEY = 'bac04dde-0a79-4218-a640-e0475fc62edc'; // Brave v1
+export const KEY = 'e9f452f4-1de1-442e-970b-cbc3aab4c173'; // Chrome v2
 export const MODAL_CLOSE_SEC = 2.5;
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -9,6 +9,33 @@ const timeout = function (s) {
   });
 };
 
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+
+    const response = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await response.json();
+    // console.log(response);
+    // console.log(data);
+
+    if (!response.ok)
+      throw new Error(`🔔 ${data.message} 🔔 (${response.status})`);
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/*
 export const getJSON = async function (url) {
   try {
     const fetchPro = fetch(url);
@@ -49,3 +76,4 @@ export const sendJSON = async function (url, uploadData) {
     throw err;
   }
 };
+*/
